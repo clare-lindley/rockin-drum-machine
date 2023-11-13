4a3ac2b0b10fc00bf8906abe25cd8ec99832768a
Clare Lindley <clarelindley@Clares-MacBook-Air.local>
2023-11-13 12:38:49 +0000

    Work in Progress: copy over all work so far into fresh next.js installation

---
+++ rockin-drum-machine/app/AudioRecorder.tsx
@@ -0,0 +1,107 @@
+/**
+ * Media Recorder copied from here:https://blog.logrocket.com/how-to-create-video-audio-recorder-react/#mediarecorder-api
+ */
+
+import { useState, useRef } from "react";
+
+const mimeType = "audio/webm"
+
+type RecordingStatus = 'recording' | 'inactive' | 'paused'
+
+const AudioRecorder = () => {
+    const [permission, setPermission] = useState<boolean>(false)
+    const mediaRecorder = useRef<MediaRecorder | null>(null)
+    const [recordingStatus, setRecordingStatus] = useState<RecordingStatus>("inactive")
+    const [stream, setStream] = useState<MediaStream | null>(null)
+    const [audioChunks, setAudioChunks] = useState<Blob[] | []>([])
+    const [audio, setAudio] = useState<string | null>(null)
+
+    const startRecording = async () => {
+        setRecordingStatus("recording")
+        //create new Media recorder instance using the stream
+        if(stream){
+            const media = new MediaRecorder(stream, { mimeType })
+            //set the MediaRecorder instance to the mediaRecorder ref
+            if(mediaRecorder){
+                mediaRecorder.current = media
+                //invokes the start method to start the recording process
+                mediaRecorder.current.start()
+                let localAudioChunks: Blob[] = []
+                mediaRecorder.current.ondataavailable = (event) => {
+                   if (typeof event.data === "undefined") return
+                   if (event.data.size === 0) return
+                   localAudioChunks.push(event.data)
+                };
+                setAudioChunks(localAudioChunks)
+            }
+
+        }
+
+    }
+
+    const stopRecording = () => {
+        setRecordingStatus("inactive")
+        if(mediaRecorder && mediaRecorder.current){
+            //stops the recording instance
+            mediaRecorder.current.stop()
+            mediaRecorder.current.onstop = () => {
+            //creates a blob file from the audiochunks data
+            const audioBlob = new Blob(audioChunks, { type: mimeType })
+            //creates a playable URL from the blob file.
+            const audioUrl = URL.createObjectURL(audioBlob);
+            setAudio(audioUrl)
+            setAudioChunks([])
+            }
+        }
+      }
+
+    const getMicrophonePermission = async () => {
+        if ("MediaRecorder" in window) {
+            try {
+                const streamData: MediaStream = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: false,
+                });
+                setPermission(true);
+                setStream(streamData);
+            } catch (err: any) {
+                alert(err.message);
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your browser.");
+        }
+    };
+    return (
+        <div className="audio-recorder">
+            <h2>Audio Recorder</h2>
+            <main>
+                <div className="audio-controls">
+                    {!permission ? (
+                    <button onClick={getMicrophonePermission} type="button">
+                        Get Microphone
+                    </button>
+                    ) : null}
+                    {permission && recordingStatus === "inactive" ? (
+                    <button onClick={startRecording} type="button">
+                        Start Recording
+                    </button>
+                    ) : null}
+                    {recordingStatus === "recording" ? (
+                    <button onClick={stopRecording} type="button">
+                        Stop Recording
+                    </button>
+                    ) : null}
+                </div>
+                {audio ? (
+                <div className="audio-player">
+                    <audio src={audio} controls></audio>
+                    <a download href={audio}>
+                        Download Recording
+                    </a>
+                </div>
+                ) : null}
+            </main>
+        </div>
+    );
+};
+export default AudioRecorder;
--- rockin-drum-machine/app/page.tsx
+++ rockin-drum-machine/app/page.tsx
@@ -1,113 +1,151 @@
-import Image from 'next/image'
+"use client"
+
+import React, { useEffect, useState } from "react";
+import AudioRecorder from "./AudioRecorder";
+
+interface Drum {
+    id: string,
+    audioFileUrl: string,
+    key: string,
+    name: string
+} 
+
+interface DrumMachineProps {
+  drums: Drum[]
+}
+
+interface DrumProps {
+  drum: Drum
+  onDrumClick: () => void
+}
+
+/**
+ * 
+ * @todo
+ * 1. Why do we have to cleanup with usestate and remove the event listener like this?
+ * https://www.pluralsight.com/guides/event-listeners-in-react-components
+ * 
+ * 2. Find out if possible to share refs between DrumPad and DrumMachine - avoid document.getElementById 
+ * 
+ */
 
 export default function Home() {
+  const drums = [
+    {
+      id: "4be47b47-ed3e-46cd-80e0-64cbcc957ef8",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+      key: "Q",
+      name: "Heater 1"
+    },    
+    {
+      id: "a9dc6d5d-5a90-40f2-9aad-96b1a2a29e24",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+      key: "W",
+      name: "Heater 2"
+    },
+    {
+      id: "06c92b88-f0dc-441b-867b-00efded31e98",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+      key: "E",
+      name: "Heater 3"
+    },    
+    {
+      id: "565d2d0c-d30d-42da-a9ea-bc047d92f477",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+      key: "A",
+      name: "Heater 4"
+    },    
+    {
+      id: "bd42c49b-afa8-4492-9f9f-7723c1f63731",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+      key: "S",
+      name: "Clap"
+    },
+    {
+      id: "dad313e0-5237-4e14-bb40-31a49e4f7a57",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+      key: "D",
+      name: "Open HH"
+    },
+    {
+      id: "a3c1261a-0bfd-46a8-adb3-12c3506a3338",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+      key: "Z",
+      name: "Kick 'n' Hat"
+    },
+    {
+      id: "74a6b082-8ee3-4ac0-957a-de5612a1e049",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+      key: "X",
+      name: "Kick"
+    },
+    {
+      id: "22aa641a-a575-4579-9c4c-f707c67cd858",
+      audioFileUrl: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+      key: "D",
+      name: "Closed HH"
+    }
+  ]
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          OOOOOOHHHH WEEEE AN EDIT!
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <AudioRecorder />
+      <DrumMachine drums={drums} />
+    </>
   )
 }
+
+function DrumMachine(props: DrumMachineProps) {
+
+  const { drums } = props
+
+  const [currentDrum, setCurrentDrum] = useState<string | undefined>()
+
+  function playDrumPad(drum: Drum){
+    setCurrentDrum(drum.name)
+    const audio = document.getElementById(drum.key) as HTMLAudioElement
+    audio?.play()
+  }
+
+  function handleDrumClick(drum: Drum){
+    playDrumPad(drum)
+  }
+
+  function handleDrumKeyPress(event: KeyboardEvent){
+    const selected = event.key.toUpperCase()
+    const drum = drums.find((drum) => drum.key === selected)
+    if(drum){
+      playDrumPad(drum)
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('keydown', handleDrumKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleDrumKeyPress);
+    };
+  }, []);
+ 
+  
+  return (
+    <main id="drum-machine">
+      {currentDrum && <div id="display">Sound: {currentDrum}</div>}
+     {drums.map((drum, index) =>  <DrumPad drum={drum} key={drum.id} onDrumClick={() => handleDrumClick(drum)}></DrumPad>)}
+    </main>
+  );
+}
+
+function DrumPad(props: DrumProps) {
+
+  const { drum, onDrumClick } = props
+
+  return (
+    <div 
+    className="drum-pad"
+    onClick={(event: React.UIEvent<HTMLDivElement>) => onDrumClick()}
+    >
+      {drum.key}
+      <audio className="clip" id={drum.key} src={drum.audioFileUrl}></audio>
+    </div>
+  )
+}
+
