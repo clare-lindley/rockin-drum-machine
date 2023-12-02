97301d19f00e2e3599b53e450c783a2ce9762e4e
Clare Lindley <clare.lindley@gmail.com>
2023-12-02 16:11:38 +0000

    WIP

---
+++ app/create-drum-machine/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from "react";
+
+/**
+ * SPIKE 2!  use app router to create the ‘create-drum-machine’ page.   ✅ 
+ * app/create-drum-machine/page.tsx = the UI for the route '/create-drum-machine' ie the Create Drum Machine page
+ */
+export default function CreateDrumMachine() {
+
+  return (
+    <>
+     <h1>Create Drum Machine</h1>
+    </>
+  )
+}
+
+
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 import React from "react";
 import AudioRecorder from "./components/AudioRecorderV2";
 
+/**
+ * app/page.tsx = the UI for the route '/' ie the homepage
+ */
 export default function Home() {
 
   return (
